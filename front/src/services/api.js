c72be4bff4188e50302b80cc438da3341eed2fb9
RahulSahani78 <rahulsahani7887@gmail.com>
2025-08-09 20:36:20 +0530

    new commit

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: prprocess.env.REACT_APP_BACKEND_URL, // or your deployed backend URL
+  baseURL: process.env.REACT_APP_BACKEND_URL, // or your deployed backend URL
 });
 
 export default api;
